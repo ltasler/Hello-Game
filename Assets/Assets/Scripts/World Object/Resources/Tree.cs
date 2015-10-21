e4b44ae6c4baaea9893ae77c586a44ccc26eacc7
Luka Tasler <tasler.luka@gmail.com>
2015-10-21 23:41:49 +0200

    commit

---
+++ Assets/Assets/Scripts/World Object/Resources/Tree.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : MonoBehaviour {
+
+	//how much wood a tree still contains
+	public int resourceCount;
+
+	//how long does worker has to "hit" it till he collects a resource
+	public float resistance;
+
+	private float workNeeded = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int ChopTree(float workerPower) {
+		if(workNeeded == 1) {
+			workNeeded = 0.0f;
+			return 1;
+		}
+		else {
+			workNeeded += (Time.deltaTime * workerPower) / resistance;
+			return 0;
+		}
+	}
+}
